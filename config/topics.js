5a886f6581da90178931f15672f7d22c99d7fa0c
Peter van Westen <info@regularlabs.com>
2017-06-13 09:21:14 +0200

    Adds separate config files Adds translations Improves html Improves styling

---
+++ config/topics.js
@@ -0,0 +1,18 @@
+// change this if you want to change the default list of topics
+var topics = {
+	'Agile'   : [
+		'Agile Tools',
+		'Agile',
+		'Automated Testing',
+		'Build quality in',
+		'Burndown Charts',
+		'Continuous Delivery',
+		'Continuous Improvement',
+		'Daily Stand-up',
+		'Discovery'
+	],
+	'Freeform': [
+		'Add your own topics',
+		'One per line'
+	]
+};
